63ad02ad50e80264ef12403252c5a897e917e53f
InteractiveTimmy <interactivetimmy@gmail.com>
2020-09-25 17:40:30 -0400

    chore: initialize the base project

---
+++ src/example-utils.ts
@@ -0,0 +1,22 @@
+/**
+ * Generic method.
+ *
+ * @returns - The product of x and y.
+ */
+export function methodA(): number {
+  const x = 1;
+  const y = 2;
+
+  return x * y;
+}
+
+/**
+ * Generic method.
+ *
+ * @param x - The initial value of x.
+ * @param y - The initial value of y.
+ * @returns - The product of x and y.
+ */
+export function methodB(x: number, y: number): number {
+  return x * y;
+}
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * An example library for the typescript-template.
+ *
+ * @packageDocumentation
+ */
+
+import {
+  methodA,
+  methodB,
+} from './example-utils';
+import Example from './example-class';
+
+export {
+  Example,
+};
